d4dfb64b4094f9aa32cf4328154037bf63e389cc
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-09-13 17:17:41 +0900

    Update real_account
    모의투자 하는 중이라 실전 투자 계좌번호는 아직 안 필요하므로 공백으로 비워둠

--- library/cf.py
+++ library/cf.py
@@ -20,7 +20,7 @@ imi1_db_name = "AiInvestmentBot"+str(imi1_simul_num)+"_imi1"
 
 
 # 아래는 실전 투자 계좌번호를 넣는다.
-real_account="7009199231"
+real_account=""
 real_simul_num=1
 real_db_name="AiInvestmentBot"+str(real_simul_num)
 
