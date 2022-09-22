47110203361df987852998ab05e8ef051d32e1c4
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-09-22 23:09:41 +0900

    Create https://opendart.fss.or.kr/ (사이트) crawling function
    https://opendart.fss.or.kr/ 사이트에서 크롤링 해주는 소스입니다
    dart_api_key를 발급 받아서 cf.py에서 dart_api_key 변수를 생성해줍니다.

--- library/cf.py
+++ library/cf.py
@@ -40,8 +40,8 @@ TR_REQ_TIME_INTERVAL_LONG = 1
 # api를 최대 몇 번까지 호출 하고 봇을 끌지 설정 하는 옵션
 max_api_call = 999
 
-# dart api key (고급클래스에서 소개)
-dart_api_key = ''
+# dart api key 
+dart_api_key = '238e0d032ab2ef85eeca2251fc8aa0be4bbc360f'
 
-# etf 사용 여부 (고급클래스에서 소개)
+# etf 사용 여부 
 use_etf = False
