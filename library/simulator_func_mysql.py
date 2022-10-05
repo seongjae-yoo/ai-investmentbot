038201430f20d29f434f7b775c8a0215501b8264
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-10-06 01:24:25 +0900

    Update self.simul_start_date variable and python simulator.py (cmd Execution)

--- library/simulator_func_mysql.py
+++ library/simulator_func_mysql.py
@@ -326,7 +326,7 @@ class simulator_func_mysql:
                     # 매도 리스트 설정 알고리즘 번호(Absolute Momentum code ver)
                     self.sell_list_num = 5
                     # 시뮬레이팅 시작 일자(분 별 시뮬레이션의 경우 최근 1년 치 데이터만 있기 때문에 start_date 조정 필요)
-                    self.simul_start_date = "20220901"
+                    self.simul_start_date = "20220821"
                     # n일 전 종가 데이터를 가져올지 설정 (ex. 20 -> 장이 열리는 날 기준 20일 이니까 기간으로 보면 약 한 달, 250일->1년)
                     self.day_before = 20 # 단위 일 (모멘텀에서 현재가랑 몇 일전의 종가와 비교할지)
                     # n일 전 종가 대비 현재 종가(현재가)가 몇 프로 증가 했을 때 매수, 몇 프로 떨어졌을 때 매도 할 지 설정(0으로 설정 시 단순히 증가 했을 때 매수, 감소 했을 때 매도)
