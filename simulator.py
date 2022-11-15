14d6d3da78d9a9b56c3e078f304d5d68d3606b88
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2022-11-15 21:28:59 +0900

    create logs

--- simulator.py
+++ simulator.py
@@ -10,13 +10,13 @@ class Simulator:
         # 시뮬레이터 번호 설정
         self.simul_num = int(input("시뮬레이팅 할 알고리즘 번호를 입력 하세요: "))
 
-        # self.simul_reset 설정
+        # self.simul_reset 설정 
         #       'y'  :  self.simul_num 에서 설정한 번호에 해당 하는 시뮬레이터 데이터베이스를 초기화 하고 처음 부터 실행
         #       'n' : self.simul_num 에서 설정한 번호에 해당 하는 시뮬레이터 데이터베이스를 초기화 하지 않고 이어서 실행
         #                    ex) 2020년 01월 01일까지 시뮬레이터를 마쳤는데, 그 이후로 연달아서 2020년 01월 02일 부터 시뮬레이터 테스트를 하고 싶은 경우
 
         option = str(input("시뮬레이팅 데이터베이스 초기화 여부 : (y or n) "))
-
+       
         if option == 'y':
             self.simul_reset = 'reset'
         elif option == 'n':
