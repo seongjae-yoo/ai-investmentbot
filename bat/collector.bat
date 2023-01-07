807dc59162e68f1e0431b5da6303445a0e9df841
YooSeongJae <54341259+SEONGJAE-YOO@users.noreply.github.com>
2023-01-08 02:07:26 +0900

    Update collector.bat

--- bat/collector.bat
+++ bat/collector.bat
@@ -1,46 +1,25 @@
 @Echo off
 @Echo collector Start
 set x=0
-set time_unit=1
-set max=1200
-set target_window="collector"
-set file="%~dp0\..\collector_v3.py"
-set activate_path="C:\ProgramData\Miniconda3\Scripts\activate.bat"
-IF EXIST %activate_path% (
-    call %activate_path% py37_32
-) ELSE (
-    echo Cannot find %activate_path%
-    pause
-    exit 1
-)
-IF NOT EXIST %file% (
-    echo Cannot find %file%
-    pause
-    exit 1
-)
-goto start_point
+call "C:\ProgramData\Miniconda3\Scripts\activate.bat" py37_32
+@taskkill /f /im python.exe 2> NUL
 
+:repeat
+@tasklist | find "python.exe" /c > NUL
+IF %ErrorLevel%==1 goto 1
+IF NOT %ErrorLevel%==1 goto 0
 
-:kill_point
+:0
+set /a x=%x%+1
+echo x : %x%
+::echo max : %max%
+IF %x%==%max% @taskkill /f /im "python.exe"
+goto repeat
+
+:1
 set x=0
-echo Killing collector...
-@taskkill /pid %process_id% /f 2> nul
+set max=5000
 
-
-:start_point
-@taskkill /f /im "opstarter.exe" 2> nul
-echo Starting a new session...
-start "%target_window%" python %file%
-for /F "tokens=2 delims=," %%A in ('tasklist /fi "imagename eq python.exe" /v /fo:csv ^| findstr /r /c:".*%target_window%[^,]*$"') do set process_id=%%A
-
-
-:count_point
-@timeout /t %time_unit% /nobreak > nul
-echo %x%
-tasklist /fi "imagename eq python.exe" /v /fo:csv | findstr /r /c:".*%target_window%[^,]*$" > nul
-IF errorlevel 1 goto kill_point
-IF %x% GEQ %max% (
-    goto kill_point
-)
-set /A "x+=time_unit"
-goto count_point
+start python "%~dp0/../collector_v3.py"
+timeout 5 > NUL
+goto repeat
